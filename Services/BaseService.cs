25c3667d4b176787649ab397b648a2a2d16db2c9
Levi080 <leviribeiro0800@gmail.com>
2025-08-13 08:32:27 -0300

    feat: correção na segunda controller

---
+++ Services/BaseService.cs
@@ -0,0 +1,34 @@
+﻿using ProvaPub.Models;
+using ProvaPub.Repository;
+
+namespace ProvaPub.Services
+{
+    public class BaseService<TEntity> where TEntity : class
+    {
+        protected readonly TestDbContext _ctx;
+        protected const int PageSize = 10;
+
+        public BaseService(TestDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public PagedList<TEntity> ListItems(int page)
+        {
+            if (page < 1) page = 1;
+
+            var totalCount = _ctx.Set<TEntity>().Count();
+            var items = _ctx.Set<TEntity>()
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize)
+                .ToList();
+
+            return new PagedList<TEntity>()
+            {
+                HasNext = (page * PageSize) < totalCount,
+                TotalCount = totalCount,
+                Items = items
+            };
+        }
+    }
+}
